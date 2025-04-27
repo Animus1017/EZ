54125d5dd52561b46267492fd7110c93e76a1e19
Animus1017 <slayergaming2k20@gmail.com>
2025-04-27 20:07:39 +0530

    final

--- src/components/core/Homepage/USP.jsx
+++ src/components/core/Homepage/USP.jsx
@@ -4,53 +4,65 @@ import { uspData } from "../../../data/usp";
 const USP = () => {
   return (
     <div className="my-12 relative">
-      <div className="[clip-path:polygon(29%_0,100%_7%,100%_96%,72%_100%,0_96%,0_5%)] bg-darkBlue/10 absolute inset-0 scale-y-110"></div>
+      <div className="[clip-path:polygon(29%_0,100%_7%,100%_96%,72%_100%,0_96%,0_5%)] bg-darkBlue/10 absolute inset-0 scale-y-100 lg:scale-y-110"></div>
       <div className="max-w-maxContent w-11/12 mx-auto flex flex-col text-darkBlue gap-12">
         <h2 className="text-darkBlue text-5xl font-semibold text-center">
           What makes us so special?
         </h2>
-        <div className="flex gap-8">
+        <div className="flex flex-col lg:flex-row gap-20 lg:gap-6 xl:gap-8">
           {/* left usp */}
           <div className="flex flex-col gap-6">
             <h3 className="text-[32px] font-semibold text-center ">
               The 10-20-30 Rule at EZ
             </h3>
-            <div className=" flex gap-8 items-center">
+            <div className=" flex gap-3 sm:gap-4 xl:gap-8 items-center justify-between">
+              {/* dot line */}
               <div className="flex flex-col items-center gap-2">
                 <div className="rounded-full bg-darkBlue w-4 aspect-square"></div>
-                <div className="bg-darkBlue/60 w-[5px] rounded-lg h-40"></div>
+                <div className="bg-darkBlue/60 w-[5px] rounded-lg h-24 sm:h-40 lg:h-24 xl:h-28"></div>
                 <div className="rounded-full bg-darkBlue w-4 aspect-square"></div>
-                <div className="bg-darkBlue/60 w-[5px] rounded-lg h-40"></div>
+                <div className="bg-darkBlue/60 w-[5px] rounded-lg h-24 sm:h-40 lg:h-24 xl:h-28"></div>
                 <div className="rounded-full bg-darkBlue w-4 aspect-square"></div>
               </div>
+              {/* minutes */}
               <div className="flex flex-col gap-[72px]">
-                <p className="text-[80px] font-bold">
-                  10<span className="text-[28px]">Minutes</span>
+                <p className="text-6xl sm:text-[80px] lg:text-6xl xl:text-[80px] font-bold">
+                  10
+                  <span className="text-2xl sm:text-[28px] lg:text-2xl xl:text-[28px]">
+                    Minutes
+                  </span>
                 </p>
-                <p className="text-[80px] font-bold">
-                  20<span className="text-[28px]">Minutes</span>
+                <p className="text-6xl sm:text-[80px] lg:text-6xl xl:text-[80px] font-bold">
+                  20
+                  <span className="text-2xl sm:text-[28px] lg:text-2xl xl:text-[28px]">
+                    Minutes
+                  </span>
                 </p>
-                <p className="text-[80px] font-bold">
-                  30<span className="text-[28px]">Minutes</span>
+                <p className="text-6xl sm:text-[80px] lg:text-6xl xl:text-[80px] font-bold">
+                  30
+                  <span className="text-2xl sm:text-[28px] lg:text-2xl xl:text-[28px]">
+                    Minutes
+                  </span>
                 </p>
               </div>
-              <div className="flex flex-col justify-between h-full">
-                <div className="text-[28px] bg-richblack-5 py-4 w-72 grid place-items-center text-center rounded-xl h-28">
-                  Acknowledge <br /> Request
+              {/* usp box */}
+              <div className="flex flex-col justify-between h-full gap-3 sm:gap-20 lg:gap-3 xl:gap-0">
+                <div className="text-2xl sm:text-[28px] lg:text-xl xl:text-[28px] bg-richblack-5 p-3 sm:p-4 lg:p-3 xl:p-0 py-0 xl:py-4 w-full sm:w-72 lg:w-full xl:w-72 grid place-items-center text-center rounded-xl h-28">
+                  Acknowledge <br className="" /> Request
                 </div>
-                <div className="text-[28px] bg-richblack-5 py-4 w-72 grid place-items-center rounded-xl h-28 ">
-                  Allocate Experts
+                <div className="text-2xl sm:text-[28px] lg:text-xl xl:text-[28px] bg-richblack-5 p-3 sm:p-4 lg:p-3 xl:p-0 py-0 xl:py-4 w-full sm:w-72 lg:w-full xl:w-72 grid place-items-center text-center rounded-xl h-28">
+                  Allocate <br className="sm:hidden inline" /> Experts
                 </div>
-                <div className="text-[28px] bg-richblack-5 py-4 w-72 grid place-items-center rounded-xl h-28 ">
-                  Begin Assignment
+                <div className="text-2xl sm:text-[28px] lg:text-xl xl:text-[28px] bg-richblack-5 p-3 sm:p-4 lg:p-3 xl:p-0 py-0 xl:py-4 w-full sm:w-72 lg:w-full xl:w-72 grid place-items-center text-center rounded-xl h-28">
+                  Begin <br className="sm:hidden inline" /> Assignment
                 </div>
               </div>
             </div>
           </div>
           {/* middle line */}
-          <div className="border-borderBlue border"></div>
+          <div className="border-borderBlue border hidden lg:block"></div>
           {/* right usp */}
-          <div className="grid grid-rows-2 grid-cols-2 flex-grow gap-3.5">
+          <div className="grid grid-cols-1 sm:grid-cols-2 flex-grow gap-8 sm:gap-3.5">
             {uspData.map((item) => (
               <UspCard
                 key={item.id}
--- src/components/core/Homepage/UspCard.jsx
+++ src/components/core/Homepage/UspCard.jsx
@@ -6,10 +6,10 @@ const UspCard = ({ title, icon, description }) => {
   const linkPath = title.toLowerCase().split(" ").join("-");
 
   return (
-    <div className="perspective h-48 sm:h-52 md:h-60 w-full my-auto relative">
+    <div className="perspective h-52 w-full my-auto relative">
       <img
         src={icon}
-        className="absolute -top-[17%] left-1/2 -translate-x-2/4 z-0 scale-125 sm:scale-150"
+        className="absolute -top-[17%] left-1/2 -translate-x-2/4 z-0 scale-150"
       />
       <Link
         to={linkPath}
@@ -18,33 +18,31 @@ const UspCard = ({ title, icon, description }) => {
       >
         <div className="relative w-full h-full transition-transform duration-500 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)] group-focus:[transform:rotateY(180deg)]">
           {/* before flip */}
-          <div className="absolute inset-0 bg-gradient-hero w-full h-full text-white rounded-xl p-2 sm:p-3 md:p-4 [backface-visibility:hidden] flex flex-col justify-between shadow-[0_10px_25px_-5px_rgba(31,41,55,0.5)]">
+          <div className="absolute inset-0 bg-gradient-hero w-full h-full text-white rounded-xl p-3 [backface-visibility:hidden] flex flex-col justify-between shadow-[0_10px_25px_-5px_rgba(31,41,55,0.5)]">
             <div className="flex justify-between items-start transition-opacity duration-500 group-hover:opacity-0 group-focus:opacity-0">
-              <img src={icon} alt={title} className="w-8 sm:w-10 md:w-auto" />
+              <img src={icon} alt={title} className="w-10" />
               <img
                 src={commonIcons}
                 alt="common"
-                className="filter brightness-0 invert w-6 sm:w-7 md:w-8 aspect-square"
+                className="filter brightness-0 invert w-7 aspect-square"
               />
             </div>
-            <div className="w-2/3 text-center mx-auto text-lg sm:text-xl md:text-2xl font-medium transition-opacity duration-500 group-hover:opacity-0 group-focus:opacity-0">
+            <div className="w-2/3 text-center mx-auto text-xl font-medium transition-opacity duration-500 group-hover:opacity-0 group-focus:opacity-0">
               {title}
             </div>
           </div>
 
           {/* after flip */}
-          <div className="absolute inset-0 bg-gradient-hero w-full h-full text-white rounded-xl p-2 sm:p-3 md:p-4 [transform:rotateY(180deg)] [backface-visibility:hidden] flex flex-col justify-between shadow-[0_10px_25px_-5px_rgba(31,41,55,0.5)]">
-            <div className="flex flex-col gap-2 md:gap-3 transition-opacity duration-500 opacity-0 group-hover:opacity-100 group-focus:opacity-100 delay-150">
-              <h4 className="text-center text-base sm:text-lg md:text-xl font-medium">
+          <div className="absolute inset-0 bg-gradient-hero w-full h-full text-white rounded-xl p-3 [transform:rotateY(180deg)] [backface-visibility:hidden] flex flex-col justify-between shadow-[0_10px_25px_-5px_rgba(31,41,55,0.5)]">
+            <div className="flex flex-col gap-3 lg:gap-1  xl:gap-3 transition-opacity duration-500 opacity-0 group-hover:opacity-100 group-focus:opacity-100 delay-150">
+              <h4 className="text-center text-lg lg:text-base xl:text-lg font-medium">
                 {title}
               </h4>
-              <p className="text-richblack-25 text-xs sm:text-sm md:text-base">
-                {description}
-              </p>
+              <p className="text-richblack-25 text-sm">{description}</p>
             </div>
             <Link
               to={linkPath}
-              className="text-xs sm:text-sm text-white underline text-left transition-opacity duration-500 opacity-0 group-hover:opacity-100 group-focus:opacity-100 delay-150"
+              className="text-sm lg:text-xs xl:text-sm text-white underline text-left transition-opacity duration-500 opacity-0 group-hover:opacity-100 group-focus:opacity-100 delay-150"
             >
               Read More
             </Link>
